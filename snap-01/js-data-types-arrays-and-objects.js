c5827e608451c675a47763a53c8cbc8ef91b85b1
Scott Coons <scoons@cnm.edu>
2021-07-16 09:23:34 -0600

    initial commit

---
+++ snap-01/js-data-types-arrays-and-objects.js
@@ -0,0 +1,22 @@
+console.log('hello world')
+
+let name = 'Sue'
+console.log(name)
+let age = 18
+console.log(age)
+
+// array
+let people = ['Marty', 'Anais', 'Jonathan', 'Andrew']
+console.log(people)
+let ages = [4,5,9,2]
+console.log(people[1])
+
+// objects
+
+let person = {
+    name: 'Anais',
+    status: 'bootcamper',
+    hobby: 'cooking'
+}
+console.log(person)
+console.log(person.status)
---
+++ snap-01/snap-01.js
@@ -0,0 +1,25 @@
+//SNAP CHALLENGE 7/14
+
+function multiplyTwoNumbers (number1, number2) {
+    console.log("first Number", number1)
+    console.log("second Number", number2)
+    var result = number1 * number2
+    return result
+}
+let firstNumber = 20
+
+
+
+
+// Function 2
+
+const value = "a string"
+`I am ${value}, Isn't javascript fun?`
+
+
+// Function 2a
+
+console.log(combineTwoStrings) (string1 " Javascipt is to carpet as " string2 "Java is to car")
+
+
+
