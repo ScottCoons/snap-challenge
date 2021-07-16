2c2d486c9ea24dbfeab6f089754195eae76a878e
Scott Coons <scoons@cnm.edu>
2021-07-16 09:54:53 -0600

    added functions and stuff

--- snap-01/js-data-types-arrays-and-objects.js
+++ snap-01/js-data-types-arrays-and-objects.js
@@ -6,6 +6,7 @@ let age = 18
 console.log(age)
 
 // array
+
 let people = ['Marty', 'Anais', 'Jonathan', 'Andrew']
 console.log(people)
 let ages = [4,5,9,2]
@@ -13,10 +14,37 @@ console.log(people[1])
 
 // objects
 
-let person = {
+let person1 = {
     name: 'Anais',
     status: 'bootcamper',
-    hobby: 'cooking'
+    hobby: 'cooking',
+    code: function() {console.log('develop website')}
 }
-console.log(person)
-console.log(person.status)
+
+let person2 = {
+    name: 'Marty',
+    status: 'bootcamper',
+    hobby: 'snowboarding',
+    code: function() {console.log('develop website')}
+}
+
+let person3 = {
+    name: 'Jonathan',
+    status: 'bootcamper',
+    hobby: 'hiking',
+    code: function() {console.log('develop website')}
+}
+
+let person4 = {
+    name: 'Andrew',
+    status: 'bootcamper',
+    hobby: 'board games',
+    code: function() {console.log('develop website')}
+}
+
+let bootcamp = [ person1, person2, person3, person4]
+console.log(bootcamp[2])
+console.log(bootcamp[2].hobby)
+
+console.log(bootcamp[2].name + ' likes ' + bootcamp[2].hobby)
+
